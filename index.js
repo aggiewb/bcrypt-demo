435bc877cfb58b36a36dc1e83a2292f560d0c754
AggieWB <usmaggie23@gmail.com>
2020-11-28 12:05:32 -0800

    Compare hash to plain text password using bcrypt compare
    method

--- index.js
+++ index.js
@@ -4,4 +4,12 @@ const saltRound = 10;
 const password = 'password';
 bcrypt.hash(password, saltRound, (error, hash) => {
     console.log(hash);
+    bcrypt.compare(password, hash, (err, isMatch) => {
+        console.log(isMatch, "is true");
+    });
+
+    bcrypt.compare('passworf', hash, (err, isMatch) => {
+        console.log(isMatch, "is false");
+    });
 });
+
